af85494c51cb27ee54dcb0f261d7f2be8b57eb7a
ChinMing Kuo <>
2021-05-18 23:53:35 +0800

    feature: 新增 404 頁面

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    404 not found
-  </div>
+  <v-container fill-height>
+    <v-row>
+      <v-col class="text-center">
+        <h1 class="primary--text display-2">歐哦, 404</h1>
+        <p>您找尋的網頁不存在</p>
+        <v-btn outlined color="primary" exact>帶我離開這裡</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'notFuound',
+};
+</script>
